f06ca8bf97687df436faa240ad29fd003ff075ff
MarkWieczorek <mark.a.wieczorek@gmail.com>
2015-03-11 23:44:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+SHTOOLS homebrew tap
+--------------------
+
+This repo is the SHTOOLS homebrew tap, which can be used to install SHTOOLS on OSX.
+
+This is a work in progress, so if you know how to set this up correctly, let me know!
+
+Installation instructions
+-------------------------
+
+To install brew, use the following command in the terminal window:
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+If brew is already installed, update it and the previously installed packages using
+
+    brew update
+    brew upgrade
+ 
+To download the SHTOOLS tap, enter
+
+    brew tap SHTOOLS/shtools
+
+
+For more information
+--------------------
+SHTOOLS web site: [shtools.ipgp.fr](http://shtools.ipgp.fr)<br>
+SHTOOLS at GitHub: [SHTOOLS/SHTOOLS](https://github.com/SHTOOLS/SHTOOLS)<br>
+SHTOOLS wiki: [Wiki](https://github.com/SHTOOLS/SHTOOLS/wiki)<br>
+SHTOOLS Twitter developer feed: [Twitter](https://twitter.com/SH_tools)
---
+++ shtools.rb
@@ -0,0 +1,38 @@
+require "formula"
+require 'fileutils'
+
+class SHTOOLS < Formula
+  homepage "https://shtools.ipgp.fr" # used by `brew home example-formula`.
+  url "https://github.com/SHTOOLS/SHTOOLS/archive/v3.0.tar.gz" 
+  version "3.0"
+  sha256 "e97ee5262a021c7ffe99535b3dd6a35ccc3d92b962b2f1f483969dbd0e5035f8"
+  head "https://github.com/SHTOOLS/homebrew-shtools.git"
+  
+  keg_only "For the time being, this is keg only."
+
+  depends_on :python
+  depends_on :fortran 
+  depends_on "fftw"
+
+  def install
+
+	ENV.prepend_path "PATH", "/usr/bin"
+    ENV.prepend_path "PATH", "/System/Library/Frameworks/Python.framework/Versions/Current/Extras/bin"
+    system "echo $PATH"
+	system "make"
+	system "make" "python"
+
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      Print some important notice to the user when `brew info <formula>` is
+      called or when brewing a formula.
+      This is optional. You can use all the vars like #{version} here.
+    EOS
+    s
+  end
+
+end
+
+__END__
