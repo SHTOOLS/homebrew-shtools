20174a44a2f95ba91ee0d441e6af8437bec41979
Mark Wieczorek <mark.a.wieczorek@gmail.com>
2015-03-12 16:45:44 +0100

    fix tabs again...

--- shtools.rb
+++ shtools.rb
@@ -12,11 +12,11 @@ class Shtools < Formula
 
   def install
 
-    ENV.prepend_path "PATH", "/usr/bin"
+	ENV.prepend_path "PATH", "/usr/bin"
     ENV.prepend_path "PATH", "/System/Library/Frameworks/Python.framework/Versions/Current/Extras/bin"
-    system "make"
-    system "make python"
-    #(prefix).install "examples", "index.html", "lib", "Makefile", "man", "modules", "pyshtools", "src", "www", "VERSION"
+	system "make"
+	system "make python"
+	#(prefix).install "examples", "index.html", "lib", "Makefile", "man", "modules", "pyshtools", "src", "www", "VERSION"
     
     (share/"shtools").install "examples"
     (doc).install "index.html"
@@ -30,31 +30,31 @@ class Shtools < Formula
 
   def caveats
     s = <<-EOS.undent        
-      To use SHTOOLS with gfortran, compile with
+        To use SHTOOLS with gfortran, compile with
       
-          gfortran -I/usr/local/include -m64 -fPIC -O3 -lSHTOOLS -lfftw3 -lm -llapack -lblas
+            gfortran -I/usr/local/include -m64 -fPIC -O3 -lSHTOOLS -lfftw3 -lm -llapack -lblas
       
-      To use SHTOOLS in Python:
+        To use SHTOOLS in Python:
       
-          import sys
-          sys.path.append('/usr/local/lib/python2.7/site-packages')
-          (alternatively, add /usr/local/lib/python2.7/site-packages to the system PYTHONPATH)
-          import pyshtools as shtools
-          
-      To run the test/example suite:  
-          
-          cd /usr/local/share/shtools/examples/fortran
-          make fortran-tests (NOTE: the Makefile needs to be updated to use -I/usr/local/include)
-          cd /usr/local/share/shtools/examples/fortran
-          make python-tests
-          
-      Local SHTOOLS web documentation:
-          
-          /usr/local/share/doc/shtools/index.html
-          
-      To obtain information about the SHTOOLS brew installation, enter 
-          
-          brew info shtools
+            import sys
+            sys.path.append('/usr/local/lib/python2.7/site-packages')
+            (alternatively, add /usr/local/lib/python2.7/site-packages to the system PYTHONPATH)
+            import pyshtools as shtools
+      
+        To run the test/example suite:  
+           
+            cd /usr/local/share/shtools/examples/fortran
+            make fortran-tests (NOTE: the Makefile needs to be updated to use -I/usr/local/include)
+            cd /usr/local/share/shtools/examples/fortran
+            make python-tests
+                 
+        Local SHTOOLS web documentation:
+      
+            /usr/local/share/doc/shtools/index.html
+
+        To obtain information about the SHTOOLS brew installation, enter 
+      
+        brew info shtools
     EOS
     s
   end
