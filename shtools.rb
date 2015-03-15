099bd67aac2f2f04340338bb498b818113e291a7
MarkWieczorek <mark.a.wieczorek@gmail.com>
2015-03-14 20:27:30 -0500

    minor fixes

--- shtools.rb
+++ shtools.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Shtools < Formula
   homepage "https://shtools.ipgp.fr"
-  url "https://github.com/SHTOOLS/SHTOOLS/archive/v3.0.tar.gz" 
-  version "3.0"
+  url "https://github.com/SHTOOLS/SHTOOLS/archive/v3.0.tar.gz"
   sha256 "e97ee5262a021c7ffe99535b3dd6a35ccc3d92b962b2f1f483969dbd0e5035f8"
   head "https://github.com/SHTOOLS/homebrew-shtools.git"
-  
+
+  depends_on :python if MacOS.version <= :snow_leopard
   depends_on "gcc"
   depends_on "fftw"  => ["with-fortran"]
 
@@ -14,11 +14,10 @@ class Shtools < Formula
     # change lib and include paths in the example makefiles
   def install
 
-    ENV.prepend_path "PATH", "/usr/bin"
     ENV.prepend_path "PATH", "/System/Library/Frameworks/Python.framework/Versions/Current/Extras/bin"
     system "make"
-    system "make python"
-    
+    system "make", "python"
+
     (share/"shtools").install "examples"
     inreplace share/"shtools/examples/fortran/Makefile", "../../lib", "/usr/local/lib"
     inreplace share/"shtools/examples/fortran/Makefile", "../../modules", "/usr/local/include"
@@ -26,40 +25,38 @@ class Shtools < Formula
     #inreplace share/"shtools/examples/Makefile", "../../modules", "/usr/local/include"
     (doc).install "index.html"
     (doc).install "www"
-    (prefix).install "lib"
+    (prefix/lib).install "lib/libSHTOOLS.a"
     (include).install "modules/fftw3.mod", "modules/planetsconstants.mod", "modules/shtools.mod"
     (share).install "man"
     (lib/"python2.7/site-packages").install "pyshtools"
-    
+
   end
 
   def caveats
-    s = <<-EOS.undent        
+    s = <<-EOS.undent
         To use SHTOOLS with gfortran, compile with
-      
+
             gfortran -I/usr/local/include -m64 -fPIC -O3 -lSHTOOLS -lfftw3 -lm -llapack -lblas
-      
+
         To use SHTOOLS in Python:
-      
+
             import sys
             sys.path.append('/usr/local/lib/python2.7/site-packages')
             (alternatively, add '/usr/local/lib/python2.7/site-packages' to the system PYTHONPATH)
             import pyshtools as shtools
-      
-        To run the test/example suite:  
-           
-            cd /usr/local/share/shtools/examples/fortran
-            make
-            make run-fortran-tests
-            cd /usr/local/share/shtools/examples/fortran
-            make
-                 
+
+        To run the test/example suite:
+
+            make -C /usr/local/share/shtools/examples/fortran
+            make -C /usr/local/share/shtools/examples/fortran run-fortran-tests
+            make -C /usr/local/share/shtools/examples/python
+
         Local SHTOOLS web documentation:
-      
+
             /usr/local/share/doc/shtools/index.html
 
-        To obtain information about the SHTOOLS brew installation, enter 
-      
+        To obtain information about the SHTOOLS brew installation, enter
+
             brew info shtools
     EOS
     s
@@ -67,4 +64,3 @@ class Shtools < Formula
 
 end
 
-__END__
