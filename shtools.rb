94b94869ac21678740417f843c64f9a7d7f68801
MarkWieczorek <mark.a.wieczorek@gmail.com>
2018-05-20 19:06:41 +0200

    Version 4.2

--- shtools.rb
+++ shtools.rb
@@ -1,8 +1,8 @@
 class Shtools < Formula
   desc "Tools for working with spherical harmonics"
-  homepage "https://shtools.oca.eu"
-  url "https://github.com/SHTOOLS/SHTOOLS/archive/v4.1.tar.gz"
-  sha256 "79dd911f3482a7e577b4e0d23b1f141eff90cb3dccfe518f1968c02cdfca5448"
+  homepage "https://shtools.github.io/SHTOOLS/"
+  url "https://github.com/SHTOOLS/SHTOOLS/archive/v4.2.tar.gz"
+  sha256 "ad330e87b4977484c418e2464523da78a355f7294ac0c7278f21ed9e45366dae"
   head "https://github.com/SHTOOLS/homebrew-shtools.git"
 
   option "with-openmp", "Install the Fortran 95 OpenMP components of SHTOOLS"
@@ -20,8 +20,6 @@ class Shtools < Formula
     pkgshare.install "examples"
     inreplace pkgshare/"examples/fortran/Makefile", "../../lib", "/usr/local/lib"
     inreplace pkgshare/"examples/fortran/Makefile", "../../modules", "/usr/local/include"
-    doc.install "index.html"
-    doc.install "www"
     lib.install "lib/libSHTOOLS.a"
     include.install "modules/fftw3.mod", "modules/planetsconstants.mod", "modules/shtools.mod"
     share.install "man"
@@ -34,19 +32,10 @@ class Shtools < Formula
   def caveats
     s = <<-EOS.undent
         To use SHTOOLS with gfortran, compile with
-
             gfortran -I/usr/local/include -m64 -fPIC -O3 -lSHTOOLS -lfftw3 -lm -llapack -lblas
-
         To run the test/example suite:
-
             make -C /usr/local/share/shtools/examples/ fortran-tests
-
-        Local SHTOOLS web documentation:
-
-            /usr/local/share/doc/shtools/index.html
-
         To obtain information about the SHTOOLS brew installation, enter
-
             brew info shtools
     EOS
     s
