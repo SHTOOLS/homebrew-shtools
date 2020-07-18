e09344dce85382b29497b55f467a66a4ffccd695
MarkWieczorek <mark.a.wieczorek@gmail.com>
2020-07-19 00:06:36 +0200

    Add test

--- shtools.rb
+++ shtools.rb
@@ -1,15 +1,16 @@
 class Shtools < Formula
-  desc "Tools for working with spherical harmonics"
+  desc "Spherical Harmonic Tools"
   homepage "https://shtools.github.io/SHTOOLS/"
   url "https://github.com/SHTOOLS/SHTOOLS/archive/v4.6.2.tar.gz"
   sha256 "47a3d0678e44167c302109bd97dcc7bf6a76f045462f02ea2baff55c86ba799f"
+  license "BSD-3-Clause"
   head "https://github.com/SHTOOLS/homebrew-shtools.git"
 
   option "with-openmp", "Install the Fortran 95 OpenMP components of SHTOOLS"
   option "with-examples", "Install the Fortran 95 example code and data"
 
-  depends_on "gcc"
   depends_on "fftw"
+  depends_on "gcc"
 
   def install
     ENV.deparallelize
@@ -34,11 +35,18 @@ class Shtools < Formula
     end
   end
 
-  def caveats; <<~EOS
-    To use SHTOOLS with gfortran, compile with
-        gfortran -I/usr/local/include -m64 -fPIC -O3 -lSHTOOLS -lfftw3 -lm -framework accelerate
-    To run the test/example suite (must install with the option --with-examples):
-        make -C /usr/local/share/shtools/examples/fortran/ LAPACK="-framework accelerate" BLAS="" run-fortran-tests
+  def caveats
+    <<~EOS
+      To use SHTOOLS with your gfortran code, compile with the options
+        -I/usr/local/include -m64 -O3 -lSHTOOLS -lfftw3 -lm -framework accelerate
+      To run the test suite (must install with the option --with-examples):
+        make -C /usr/local/share/shtools/examples/fortran/ LAPACK="-framework accelerate" BLAS="" run-fortran-tests-no-timing
     EOS
+  end
+
+  test do
+    system "make", "run-fortran-tests-no-timing",
+                   "LAPACK='-framework accelerate'",
+                   "BLAS=''"
   end
 end
